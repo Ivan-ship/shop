9f8c7b39a7ee31cd2938694957078cb33d942af9
Ivan-ship <vanyfug3@gmail.com>
2026-08-26 12:50:41 +0300

    add db model

--- server/database/shema.py
+++ server/database/shema.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import String, BigInteger
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from sqlalchemy import String, BigInteger, Integer, Boolean, ForeignKey
 
 class Base(DeclarativeBase):
     pass
@@ -12,3 +12,39 @@ class Users(Base):
     first_name: Mapped[str]=mapped_column(String(50))
     last_name: Mapped[str]=mapped_column(String(50))
     telegram_id: Mapped[int]=mapped_column(BigInteger)
+
+    purchases = Mapped[list["Purchases"]]=relationship(
+        "Purchases", back_populates="user", 
+        cascade="all, delete-orphan")
+
+
+class Products(Base):
+    __tablename__ = "products"
+
+    prod_id: Mapped[int]=mapped_column(primary_key=True)
+    name: Mapped[str]=mapped_column(String(50))
+    price: Mapped[int]=mapped_column(Integer)
+    file_name: Mapped[str]=mapped_column(String(100))
+    is_active: Mapped[bool]=mapped_column(Boolean, default=False)
+
+    purchases = Mapped[list["Purchases"]]=relationship(
+        "Purchases", back_populates="product", 
+        cascade="all, delete-orphan")
+    
+
+class Purchases(Base):
+    __tablename__ ="purchases"
+
+    purchases_id: Mapped[int]=mapped_column(primary_key=True)
+    user_id: Mapped[int]=mapped_column(ForeignKey("users.user_id"))
+    prod_id: Mapped[int]=mapped_column(ForeignKey("products.prod_id"))
+
+    user = Mapped["Users"]=relationship(
+        "Users",
+        back_populates="purchases"
+    )
+
+    product = Mapped["Products"]=relationship(
+        "Products",
+        back_populates="purchases"
+    )
